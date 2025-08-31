846c6323c898f75c1b8324290dd90009cc0d089f
Fatih Kadim <skadim123@gmail.com>
2025-08-31 16:08:35 +0300

    changed model

--- nlp/model.py
+++ nlp/model.py
@@ -1,6 +1,17 @@
 from transformers import pipeline
 
-s_model = pipeline('sentiment-analysis')
+s_model = pipeline(
+    "sentiment-analysis",
+    model="cardiffnlp/twitter-roberta-base-sentiment",
+    device=0  # GPU kullanımı
+)
+
+labels = ['NEGATIVE', 'NEUTRAL', 'POSITIVE']
 
 def analyze_sentiment(text: str) -> dict:
-    return s_model(text)[0]
+    raw_result = s_model(text)[0]
+    label_idx = int(raw_result['label'].replace("LABEL_", ""))
+    return {
+        "label": labels[label_idx],
+        "score": raw_result['score']
+    }
